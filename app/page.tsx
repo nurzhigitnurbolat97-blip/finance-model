cea3508f1741bcf35ac3c0f78b95b0e43e677ecc
nurzhigitnurbolat97-blip <nurzhigitnurbolat97@gmail.com>
2026-03-07 00:27:34 +0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{padding:40,fontFamily:"sans-serif"}}>
+      <h1>Финансовый калькулятор</h1>
+      <p>Приложение работает.</p>
+    </main>
+  );
+}
