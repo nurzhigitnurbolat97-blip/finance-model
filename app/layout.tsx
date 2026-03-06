4045b246840e257a19e1ff3b9faf4ee29c0e7e08
nurzhigitnurbolat97-blip <nurzhigitnurbolat97@gmail.com>
2026-03-07 00:44:15 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-xport default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru">
       <body>{children}</body>
